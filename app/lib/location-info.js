ee285bebc4bfab126082f0b6e2b0f1b2d00dc66b
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-09-22 18:41:18 +0600

    feat: add functions for getting data

---
+++ app/lib/location-info.js
@@ -0,0 +1,27 @@
+export const getLocationData = async (lat, lon) => {
+  try {
+    const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getLocationLatLonList = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/location");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getLocationLatLon = async (locationName) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/location/${locationName}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {}
+};
